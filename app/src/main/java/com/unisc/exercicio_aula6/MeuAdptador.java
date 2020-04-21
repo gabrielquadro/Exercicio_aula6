d86df40b4601895b1dc96d48193a38d7384a84e7
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-04-21 15:45:55 -0300

    Alterar cor dos nomes e pontos do time.

--- app/src/main/java/com/unisc/exercicio_aula6/MeuAdptador.java
+++ app/src/main/java/com/unisc/exercicio_aula6/MeuAdptador.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.Map;
@@ -17,12 +18,19 @@ class MeuAdptador extends SimpleAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View v =  super.getView(position, convertView, parent);
+        TextView nomeTimes = v.findViewById(R.id.nomeTime);
+        TextView pontosTimes = v.findViewById(R.id.pontos);
+
 
         if(position % 2 == 0){
             v.setBackgroundColor(Color.GREEN);
+            nomeTimes.setTextColor(Color.BLACK);
+            pontosTimes.setTextColor(Color.BLACK);
 
         }else{
             v.setBackgroundColor(Color.YELLOW);
+            nomeTimes.setTextColor(Color.WHITE);
+            pontosTimes.setTextColor(Color.WHITE);
         }
 
         return v;
